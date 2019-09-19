e065e40e4af3ee17ce2cb4d58c38814ad8bb788d
Balint Pekker <balint.pekker@cheppers.com>
2019-09-19 17:52:04 +0200

    Issue #26919 - Add deleteContact() method
    Add method for Contact deletion.

--- src/Endpoints/ContactEndpoint.php
+++ src/Endpoints/ContactEndpoint.php
@@ -13,9 +13,12 @@ class ContactEndpoint extends MiniCrmClient
 {
 
     /**
+     * Gets back a Person based on it's contactId.
+     *
      * @param int $contactId
      *
      * @return \Cheppers\MiniCrm\DataTypes\Contact\PersonResponse
+     *
      * @throws \Exception
      */
     public function getPerson(int $contactId): PersonResponse
@@ -27,9 +30,12 @@ class ContactEndpoint extends MiniCrmClient
     }
 
     /**
+     * Gets back a Business based on it's contactId.
+     *
      * @param int $contactId
      *
      * @return \Cheppers\MiniCrm\DataTypes\Contact\BusinessResponse
+     *
      * @throws \Exception
      */
     public function getBusiness(int $contactId): BusinessResponse
@@ -41,6 +47,31 @@ class ContactEndpoint extends MiniCrmClient
     }
 
     /**
+     * Deletes a Contact.
+     *
+     * @param int $contactId
+     *
+     * @return array
+     *
+     * @throws \Exception
+     *  Throws 'Invalid response' exception when trying to delete a Contact with
+     *  a non-existing ContactId, or a Contact that is marked as primary on any
+     *  Project.
+     */
+    public function deleteContact(int $contactId): array
+    {
+        $response = $this->sendRequest(
+            'GET',
+            ContactRequest::__set_state([]),
+            "/PurgePerson/{$contactId}"
+        );
+
+        $body = $this->validateAndParseResponse($response);
+
+        return $body;
+    }
+
+    /**
      * @param int $contactId
      *
      * @return array
@@ -49,12 +80,10 @@ class ContactEndpoint extends MiniCrmClient
      */
     protected function getContact(int $contactId): array
     {
-        $path = "/Contact/{$contactId}";
-
         $response = $this->sendRequest(
             'GET',
             ContactRequest::__set_state([]),
-            $path
+            "/Contact/{$contactId}"
         );
 
         return $this->validateAndParseResponse($response);
