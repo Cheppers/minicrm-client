39cb8824aee52c879b9003e16f643fddabd8d650
Balint Pekker <balint.pekker@cheppers.com>
2019-09-24 16:46:05 +0200

    Issue #26182 - Add Client tests
    Add basic client operations test cases, add Schema endpoint test cases.

---
+++ src-dev/Test/Unit/Endpoints/SchemaEndpointTest.php
@@ -0,0 +1,228 @@
+<?php
+declare(strict_types = 1);
+
+namespace Cheppers\MiniCrm\Tests\Unit\Endpoints;
+
+use Cheppers\MiniCrm\DataTypes\Schema\SchemaResponse;
+use Cheppers\MiniCrm\Endpoints\SchemaEndpoint;
+use Cheppers\MiniCrm\MiniCrmClient;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\NullLogger;
+
+/**
+ * @group MiniCrmClient
+ *
+ * @covers \Cheppers\MiniCrm\Endpoints\SchemaEndpoint
+ */
+class SchemaEndpointTest extends TestCase
+{
+
+    /**
+     * @var MiniCrmClient
+     */
+    protected $client;
+
+    protected $clientOptions = [
+        'baseUri' => 'http://minicrm.hu',
+        'apiKey' => 'm-i-n-i',
+        'systemId' => 1234
+    ];
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void
+    {
+        $client = new Client();
+        $logger = new NullLogger();
+        $this->client = new MiniCrmClient($client, $logger);
+    }
+
+    public function casesPersonSchema()
+    {
+        $personData = [
+            'EmailType' => 'Text(1024)',
+            'PhoneType' => 'Text(1024)',
+            'FirstName' => 'Text(255)',
+            'LastName' => 'Text(255)',
+            'Email' => 'Text(128)',
+            'Phone' => 'Text(128)',
+            'Description' => 'Text(4096)',
+            'Deleted' => 'Boolean',
+        ];
+
+        return [
+            'person' => [
+                $personData,
+                $personData,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider casesPersonSchema
+     */
+    public function testGetPersonSchema(array $expected, array $response)
+    {
+        $container = [];
+        $history = Middleware::history($container);
+        $mock = new MockHandler([
+            new Response(
+                200,
+                ['Content-Type' => 'application/json; charset=utf-8'],
+                \GuzzleHttp\json_encode($response)
+            ),
+            new RequestException(
+                'Error communicating with server.',
+                new Request('GET', '/Api/R3/Schema/Person')
+            )
+        ]);
+        $handlerStack = HandlerStack::create($mock);
+        $handlerStack->push($history);
+
+        $client = new Client([
+            'handler' => $handlerStack,
+        ]);
+        $logger = new NullLogger();
+        $schema = new SchemaEndpoint($client, $logger);
+        $schema->setCredentials($this->clientOptions);
+
+        $person = $schema->getPersonSchema();
+
+        if ($expected) {
+            static::assertEquals(
+                json_encode($expected, JSON_PRETTY_PRINT),
+                json_encode($person->results, JSON_PRETTY_PRINT)
+            );
+        } else {
+            static::assertNull($person);
+        }
+    }
+
+    public function casesBusinessSchema()
+    {
+        $businessData = [
+            'EmailType' => 'Text(1024)',
+            'PhoneType' => 'Text(1024)',
+            'Id' => 'Int',
+            'ParentId' => 'Int',
+            'Name' => 'Text(255)',
+            'Email' => 'Text(128)',
+            'Phone' => 'Text(128)',
+            'Description' => 'Text(4096)',
+            'Deleted' => 'Boolean',
+        ];
+
+        return [
+            'business' => [
+                $businessData,
+                $businessData,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider casesBusinessSchema
+     */
+    public function testGetBusinessSchema(array $expected, array $response)
+    {
+        $container = [];
+        $history = Middleware::history($container);
+        $mock = new MockHandler([
+            new Response(
+                200,
+                ['Content-Type' => 'application/json; charset=utf-8'],
+                \GuzzleHttp\json_encode($response)
+            ),
+            new RequestException(
+                'Error communicating with server.',
+                new Request('GET', '/Api/R3/Schema/Business')
+            )
+        ]);
+        $handlerStack = HandlerStack::create($mock);
+        $handlerStack->push($history);
+
+        $client = new Client([
+            'handler' => $handlerStack,
+        ]);
+        $logger = new NullLogger();
+        $schema = new SchemaEndpoint($client, $logger);
+        $schema->setCredentials($this->clientOptions);
+
+        $business = $schema->getBusinessSchema();
+        if ($expected) {
+            static::assertEquals(
+                json_encode($expected, JSON_PRETTY_PRINT),
+                json_encode($business->results, JSON_PRETTY_PRINT)
+            );
+        } else {
+            static::assertNull($business);
+        }
+    }
+
+    public function casesProjectSchema()
+    {
+        $projectData = [
+            'Id' => 'Int',
+            'CategoryId' => 'Array',
+            'ContactId' => 'Int',
+            'BusinessId' => 'Int',
+            'UserId' => 'Array',
+            'Name' => 'Text(512)',
+            'StatusUpdatedAt' => 'DateTime',
+            'Deleted' => 'Int',
+        ];
+
+        return [
+            'project' => [
+                $projectData,
+                $projectData,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider casesProjectSchema
+     */
+    public function testGetProjectSchema(array $expected, array $response)
+    {
+        $container = [];
+        $history = Middleware::history($container);
+        $mock = new MockHandler([
+            new Response(
+                200,
+                ['Content-Type' => 'application/json; charset=utf-8'],
+                \GuzzleHttp\json_encode($response)
+            ),
+            new RequestException(
+                'Error communicating with server.',
+                new Request('GET', '/Api/R3/Schema/Project/1')
+            )
+        ]);
+        $handlerStack = HandlerStack::create($mock);
+        $handlerStack->push($history);
+
+        $client = new Client([
+            'handler' => $handlerStack,
+        ]);
+        $logger = new NullLogger();
+        $schema = new SchemaEndpoint($client, $logger);
+        $schema->setCredentials($this->clientOptions);
+
+        $business = $schema->getProjectSchema(1);
+        if ($expected) {
+            static::assertEquals(
+                json_encode($expected, JSON_PRETTY_PRINT),
+                json_encode($business->results, JSON_PRETTY_PRINT)
+            );
+        } else {
+            static::assertNull($business);
+        }
+    }
+}
---
+++ src-dev/Test/Unit/MiniCrmClientTest.php
@@ -0,0 +1,110 @@
+<?php
+declare(strict_types = 1);
+
+namespace Cheppers\MiniCrm\Tests\Unit;
+
+use Cheppers\MiniCrm\DataTypes\Category\CategoryRequest;
+use Cheppers\MiniCrm\MiniCrmClient;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Middleware;
+use http\Env\Response;
+use Psr\Log\NullLogger;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group MiniCrmClient
+ *
+ * @covers \Cheppers\MiniCrm\MiniCrmClient
+ */
+class MiniCrmClientTest extends TestCase
+{
+    /**
+     * @var MiniCrmClient
+     */
+    protected $client;
+
+    protected $clientOptions = [
+        'baseUri' => 'http://minicrm.hu',
+        'apiKey' => 'm-i-n-i',
+        'systemId' => 1234
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp() : void
+    {
+        $client = new Client();
+        $logger = new NullLogger();
+        $this->client = new MiniCrmClient($client, $logger);
+    }
+
+    public function testGetSetBaseUri()
+    {
+        static::assertEquals('', $this->client->getBaseUri());
+        $this->client->setBaseUri('http://minicrm.hu');
+        static::assertEquals('http://minicrm.hu', $this->client->getBaseUri());
+    }
+
+    public function testGetSetApiKey()
+    {
+        static::assertEquals('', $this->client->getApiKey());
+        $this->client->setApiKey('m-i-n-i');
+        static::assertEquals('m-i-n-i', $this->client->getApiKey());
+    }
+
+    public function testGetSetSystemId()
+    {
+        $this->client->setSystemId(1234);
+        static::assertEquals(1234, $this->client->getSystemId());
+    }
+
+    public function casesCredentials()
+    {
+        return [
+            'credential-1' => [
+                [
+                    'systemid' => 12345,
+                    'apikey' => '3EiTVRS9WCHuqjZm6c0Ov8nROxGtW7LL',
+                    'baseUri' => 'testuri',
+                ],
+                'MTIzNDU6M0VpVFZSUzlXQ0h1cWpabTZjME92OG5ST3hHdFc3TEw=',
+            ],
+            'credential-2' => [
+                [
+                    'systemid' => 98765,
+                    'apikey' => '9Lsc3OrDqBeehFCtfRiDhtay881YVCft',
+                    'baseUri' => 'nottesturi',
+                ],
+                'OTg3NjU6OUxzYzNPckRxQmVlaEZDdGZSaURodGF5ODgxWVZDZnQ=',
+            ],
+        ];
+    }
+
+    /**
+     * @param array $credentials
+     * @param string $expected
+     *
+     * @dataProvider casesCredentials
+     */
+    public function testGetCredentials(array $credentials, string $expected)
+    {
+        $this->client->setSystemId($credentials['systemid']);
+        $this->client->setApiKey($credentials['apikey']);
+        static::assertSame($expected, base64_encode($this->client->getSystemId().':'.$this->client->getApiKey()));
+    }
+
+    public function testSetCredentials()
+    {
+        $this->client->setCredentials([
+            'baseUri' => 'http://minicrm.hu',
+            'apiKey' => 'm-i-n-i',
+            'systemId' => 1234,
+            'default' => 1
+        ]);
+        static::assertEquals('http://minicrm.hu', $this->client->getBaseUri());
+        static::assertEquals('m-i-n-i', $this->client->getApiKey());
+        static::assertEquals(1234, $this->client->getSystemId());
+    }
+}
