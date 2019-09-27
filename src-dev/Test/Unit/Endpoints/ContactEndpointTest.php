645a798f4d63be98428981eefe7ea7fe5b7f1f88
Balint Pekker <balint.pekker@cheppers.com>
2019-09-27 15:15:20 +0200

    Issue #26182 - Add testGetBusiness() method
    Add method to test ContactEndpoint's getBusiness() method with empty/basic cases.

--- src-dev/Test/Unit/Endpoints/ContactEndpointTest.php
+++ src-dev/Test/Unit/Endpoints/ContactEndpointTest.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Cheppers\MiniCrm\Test\Unit\Endpoints;
 
+use Cheppers\MiniCrm\DataTypes\Contact\Business\BusinessResponse;
 use Cheppers\MiniCrm\DataTypes\Contact\Person\PersonResponse;
 use Cheppers\MiniCrm\Endpoints\ContactEndpoint;
 use Cheppers\MiniCrm\Test\Unit\MiniCrmBaseTest;
@@ -21,7 +22,7 @@ class ContactEndpointTest extends MiniCrmBaseTest
     /**
      * @return array
      */
-    public function casesTodo()
+    public function casesPerson()
     {
         return [
             'empty' => [
@@ -76,15 +77,6 @@ class ContactEndpointTest extends MiniCrmBaseTest
                 ],
                 42
             ],
-            'only-status' => [
-                PersonResponse::__set_state([
-                    'Status' => 'Open',
-                ]),
-                [
-                    'Status' => 'Open',
-                ],
-                13
-            ],
         ];
     }
 
@@ -95,7 +87,7 @@ class ContactEndpointTest extends MiniCrmBaseTest
      *
      * @throws \Exception
      *
-     * @dataProvider casesTodo
+     * @dataProvider casesPerson
      */
     public function testGetPerson(
         $expected,
@@ -114,6 +106,98 @@ class ContactEndpointTest extends MiniCrmBaseTest
         $contactEndpoint->setCredentials($this->clientOptions);
 
         $contact = $contactEndpoint->getPerson($contactId);
+
+        static::assertEquals(
+            json_encode($expected, JSON_PRETTY_PRINT),
+            json_encode($contact, JSON_PRETTY_PRINT)
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function casesBusiness()
+    {
+        return [
+            'empty' => [
+                BusinessResponse::__set_state([]),
+                [],
+                1
+            ],
+            'basic' => [
+                BusinessResponse::__set_state([
+                    'Name' => 'Test Name',
+                    'Description' => 'Test Contact Description',
+                    'Url' => '',
+                    'Industry' => null,
+                    'Region' => 'Test Region',
+                    'VatNumber' => 'Test Vat Number',
+                    'RegistrationNumber' => 'Test Registration Number',
+                    'BankAccount' => 'Test Bank Account Number',
+                    'Swift' => 'Test Swift Number',
+                    'Employees' => 99,
+                    'YearlyRevenue' => 0,
+                    'Id' => 42,
+                    'Type' => 'Test Contact Type',
+                    'Email' => 'test@em.ail',
+                    'EmailType' => 'Test Email Type',
+                    'Phone' => '123456789',
+                    'PhoneType' => 'Test Phone Type',
+                    'ParentId' => 0,
+                    'Deleted' => 0,
+                ]),
+                [
+                    'Name' => 'Test Name',
+                    'Description' => 'Test Contact Description',
+                    'Url' => '',
+                    'Industry' => null,
+                    'Region' => 'Test Region',
+                    'VatNumber' => 'Test Vat Number',
+                    'RegistrationNumber' => 'Test Registration Number',
+                    'BankAccount' => 'Test Bank Account Number',
+                    'Swift' => 'Test Swift Number',
+                    'Employees' => 99,
+                    'YearlyRevenue' => 0,
+                    'Id' => 42,
+                    'Type' => 'Test Contact Type',
+                    'Email' => 'test@em.ail',
+                    'EmailType' => 'Test Email Type',
+                    'Phone' => '123456789',
+                    'PhoneType' => 'Test Phone Type',
+                    'ParentId' => 0,
+                    'Deleted' => 0,
+                ],
+                42
+            ],
+        ];
+    }
+
+    /**
+     * @param $expected
+     * @param array $responseBody
+     * @param $contactId
+     *
+     * @throws \Exception
+     *
+     * @dataProvider casesBusiness
+     */
+    public function testGetBusiness(
+        $expected,
+        array $responseBody,
+        int $contactId
+    ) {
+        $mock = $this->createMiniCrmMock([
+            new Response(
+                200,
+                ['Content-Type' => 'application/json; charset=utf-8'],
+                \GuzzleHttp\json_encode($responseBody)
+            ),
+        ]);
+        $client = $mock['client'];
+        $contactEndpoint = new ContactEndpoint($client, new NullLogger());
+        $contactEndpoint->setCredentials($this->clientOptions);
+
+        $contact = $contactEndpoint->getBusiness($contactId);
 
         static::assertEquals(
             json_encode($expected, JSON_PRETTY_PRINT),
