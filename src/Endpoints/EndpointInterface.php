1fca716db3985df6c43092807e3e15a85baa6cda
David Olah <david.olah@cheppers.com>
2019-12-03 11:29:55 +0100

    Issue #27444 - Add comment to endpoint interface methods

--- src/Endpoints/EndpointInterface.php
+++ src/Endpoints/EndpointInterface.php
@@ -8,9 +8,24 @@ use Cheppers\MiniCrm\DataTypes\RequestBase;
 
 interface EndpointInterface
 {
+    /**
+     * @param int $id
+     *
+     * @return mixed
+     */
     public function get(int $id);
 
+    /**
+     * @param RequestBase $request
+     *
+     * @return array
+     */
     public function create(RequestBase $request): array;
 
+    /**
+     * @param RequestBase $request
+     *
+     * @return array
+     */
     public function update(RequestBase $request): array;
 }
