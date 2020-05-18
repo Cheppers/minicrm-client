9f0be0e8405a53810a169433ce32c6614c7d3f1b
Kolos Horvath <kolos.horvath@cheppers.com>
2020-05-18 16:35:49 +0200

    Issue #00000 - Add ProjectStatusUpdateRequest class

---
+++ src/DataTypes/Project/ProjectStatusUpdateRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Cheppers\MiniCrm\DataTypes\Project;
+
+use Cheppers\MiniCrm\DataTypes\RequestBase;
+
+class ProjectStatusUpdateRequest extends RequestBase 
+{
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var array
+     */
+    public $additionalProperties = [];
+  
+    /**
+    * @inheritDoc
+    */
+    public function jsonSerialize()
+    {
+    
+      $data = [];
+    
+      foreach (array_keys(get_object_vars($this)) as $key) 
+      {
+          
+          if ($key !== 'additionalProperties')
+          {
+              continue;
+          }
+          
+          foreach ($this->additionalProperties as $item => $value)
+          {
+              $data[ucfirst($item)] = $value;
+          }
+          
+      }
+    
+      return $data;
+    }
+
+}
