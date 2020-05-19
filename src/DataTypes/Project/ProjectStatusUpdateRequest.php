a9e7ed15bb229d5627d261f01ea15f0478987169
Kolos Horvath <kolos.horvath@cheppers.com>
2020-05-19 12:40:06 +0200

    Issue #00000 - Fix codesniffer fails

--- src/DataTypes/Project/ProjectStatusUpdateRequest.php
+++ src/DataTypes/Project/ProjectStatusUpdateRequest.php
@@ -6,7 +6,7 @@ namespace Cheppers\MiniCrm\DataTypes\Project;
 
 use Cheppers\MiniCrm\DataTypes\RequestBase;
 
-class ProjectStatusUpdateRequest extends RequestBase 
+class ProjectStatusUpdateRequest extends RequestBase
 {
     /**
      * @var int
@@ -17,31 +17,25 @@ class ProjectStatusUpdateRequest extends RequestBase
      * @var array
      */
     public $additionalProperties = [];
-  
+
     /**
     * @inheritDoc
     */
     public function jsonSerialize()
     {
-    
-      $data = [];
-    
-      foreach (array_keys(get_object_vars($this)) as $key) 
-      {
-          
-          if ($key !== 'additionalProperties')
-          {
-              continue;
-          }
-          
-          foreach ($this->additionalProperties as $item => $value)
-          {
-              $data[ucfirst($item)] = $value;
-          }
-          
-      }
-    
-      return $data;
-    }
 
+        $data = [];
+
+        foreach (array_keys(get_object_vars($this)) as $key) {
+            if ($key !== 'additionalProperties') {
+                continue;
+            }
+
+            foreach ($this->additionalProperties as $item => $value) {
+                $data[ucfirst($item)] = $value;
+            }
+        }
+
+        return $data;
+    }
 }
